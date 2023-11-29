1138f211d5fde350a9d2ef17fc0ea5e0ab5b8751
h0pler <kimwoojinside2@gmail.com>
2023-11-29 20:44:21 +0900

    secure ok

--- client.c
+++ client.c
@@ -1,34 +1,67 @@
+#include <arpa/inet.h>
+#include <openssl/evp.h>
+#include <openssl/rand.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
-#include <arpa/inet.h>  // ntohs()
-#include <stdio.h>      // printf(), perror()
-#include <stdlib.h>     // atoi()
-#include <string.h>     // strlen()
-#include <sys/socket.h> // socket(), connect(), send(), recv()
-#include <unistd.h>     // close()
-
-#include "helper.h" // make_client_sockaddr()
+#include "helper.h"
 
 #define BUFFER_LEN 1024
+#define IV_LEN 16  // AES-256-CBC의 IV 길이
 
 typedef struct {
   int sockfd;
   struct sockaddr_in addr;
+  unsigned char key[EVP_MAX_KEY_LENGTH];  // 암호화 키
 } Connection;
 
-int create_connection(Connection *conn, const char *hostname, int port) {
-  // Create a socket
+// 데이터를 암호화하는 함수
+int encrypt_data(const unsigned char *plaintext, int plaintext_len, unsigned char *key,
+                 unsigned char *iv, unsigned char *ciphertext) {
+  EVP_CIPHER_CTX *ctx;
+  int len;
+  int ciphertext_len;
+
+  // 컨텍스트 생성
+  if(!(ctx = EVP_CIPHER_CTX_new())) return -1;
+
+  // 암호화 초기화
+  if(1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv)) return -1;
+
+  // 데이터 암호화
+  if(1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len)) return -1;
+  ciphertext_len = len;
+
+  // 암호화 완료
+  if(1 != EVP_EncryptFinal_ex(ctx, ciphertext + len, &len)) return -1;
+  ciphertext_len += len;
+
+  // 컨텍스트 해제
+  EVP_CIPHER_CTX_free(ctx);
+
+  return ciphertext_len;
+}
+
+int create_connection(Connection *conn, const char *hostname, int port, const char *key) {
+  // 소켓 생성
   conn->sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
   if (conn->sockfd == -1) {
     perror("Error creating socket");
     return -1;
   }
 
-  // Create a sockaddr_in to specify remote host and port
+  // 키 복사
+  strncpy((char *)conn->key, key, sizeof(conn->key));
+
+  // 서버 주소 설정
   if (make_client_sockaddr(&conn->addr, hostname, port) == -1) {
     return -1;
   }
 
-  // Connect to remote server
+  // 서버에 연결
   if (connect(conn->sockfd, (struct sockaddr *)&conn->addr, sizeof(conn->addr)) == -1) {
     perror("Error connecting stream socket");
     return -1;
@@ -38,22 +71,33 @@ int create_connection(Connection *conn, const char *hostname, int port) {
 }
 
 int send_message(Connection *conn, const char *message) {
-  const size_t message_len = strlen(message);
-  if (message_len > BUFFER_LEN) {
-    perror("Error: Message exceeds maximum length\n");
+  unsigned char iv[IV_LEN];
+  unsigned char encrypted_message[BUFFER_LEN];
+  unsigned char buffer[BUFFER_LEN + IV_LEN];
+  int encrypted_len;
+
+  // 무작위 IV 생성
+  if (!RAND_bytes(iv, sizeof(iv))) {
+    perror("Error generating IV");
     return -1;
   }
 
-  // Send message to remote server
-  ssize_t sent = 0;
-  do {
-    const ssize_t n = send(conn->sockfd, message + sent, message_len - sent, 0);
-    if (n == -1) {
-      perror("Error sending on stream socket");
-      return -1;
-    }
-    sent += n;
-  } while (sent < message_len);
+  // 메시지 암호화
+  encrypted_len = encrypt_data((unsigned char *)message, strlen(message), conn->key, iv, encrypted_message);
+  if (encrypted_len == -1) {
+    perror("Error encrypting message");
+    return -1;
+  }
+
+  // IV와 암호화된 메시지 결합
+  memcpy(buffer, iv, IV_LEN);
+  memcpy(buffer + IV_LEN, encrypted_message, encrypted_len);
+
+  // 메시지 전송
+  if (send(conn->sockfd, buffer, IV_LEN + encrypted_len, 0) == -1) {
+    perror("Error sending on stream socket");
+    return -1;
+  }
 
   return 0;
 }
@@ -63,19 +107,18 @@ void close_connection(Connection *conn) {
 }
 
 int main(int argc, const char **argv) {
-  // Parse command line arguments
-  if (argc != 4) {
-    printf("Usage: ./client hostname port_num message\n");
+  if (argc != 5) {
+    printf("Usage: ./client hostname port_num encryption_key message\n");
     return 1;
   }
+
   const char *hostname = argv[1];
   const int port = atoi(argv[2]);
-  const char *message = argv[3];
-
-  printf("Sending message %s to %s:%d\n", message, hostname, port);
+  const char *key = argv[3];
+  const char *message = argv[4];
 
   Connection conn;
-  if (create_connection(&conn, hostname, port) == -1) {
+  if (create_connection(&conn, hostname, port, key) == -1) {
     return 1;
   }
 
--- server.c
+++ server.c
@@ -1,44 +1,89 @@
-
 #include <arpa/inet.h>
+#include <openssl/evp.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
 #include "helper.h"
 
 #define BUFFER_LEN 1024
+#define IV_LEN 16  // AES-256-CBC의 IV 길이
 
 typedef struct {
   int sockfd;
   int port;
   int queue;
+  unsigned char key[EVP_MAX_KEY_LENGTH];  // 암호화 키
 } Server;
 
-// 클라이언트와의 연결을 처리하는 함수
-int handle_connection(int connectionfd) {
-  printf("New connection %d\n", connectionfd);
+// 데이터를 복호화하는 함수
+int decrypt_data(const unsigned char *ciphertext, int ciphertext_len, unsigned char *key,
+                 unsigned char *iv, unsigned char *plaintext) {
+  EVP_CIPHER_CTX *ctx;
+  int len;
+  int plaintext_len;
 
-  char buffer[BUFFER_LEN];
-  memset(buffer, 0, sizeof(buffer));
+  // 컨텍스트 생성
+  if(!(ctx = EVP_CIPHER_CTX_new())) return -1;
 
-  ssize_t recvd = 0;
-  ssize_t reval;
-  do {
-    reval = recv(connectionfd, buffer + recvd, BUFFER_LEN - recvd, 0);
-    if (reval == -1) {
-      perror("Error reading stream message");
-      return -1;
-    }
-    recvd += reval;
-  } while (reval > 0);
+  // 복호화 초기화
+  if(1 != EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv)) return -1;
 
-  printf("Client %d says: '%s'\n", connectionfd, buffer);
+  // 데이터 복호화
+  if(1 != EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len)) return -1;
+  plaintext_len = len;
 
-  close(connectionfd);
+  // 복호화 완료
+  if(1 != EVP_DecryptFinal_ex(ctx, plaintext + len, &len)) return -1;
+  plaintext_len += len;
 
-  return 0;
+  // 컨텍스트 해제
+  EVP_CIPHER_CTX_free(ctx);
+
+  return plaintext_len;
 }
+
+// 클라이언트와의 연결을 처리하는 함수
+// 클라이언트와의 연결을 처리하는 함수
+int handle_connection(int connectionfd, unsigned char *key) {
+    printf("New connection %d\n", connectionfd);
+
+    unsigned char buffer[BUFFER_LEN + IV_LEN]; // IV를 포함하는 버퍼
+    unsigned char decrypted_message[BUFFER_LEN];
+    unsigned char iv[IV_LEN];
+    memset(buffer, 0, sizeof(buffer));
+    memset(decrypted_message, 0, sizeof(decrypted_message));
+    memset(iv, 0, sizeof(iv));
+
+    ssize_t recvd = 0;
+    ssize_t reval;
+    do {
+        reval = recv(connectionfd, buffer + recvd, BUFFER_LEN + IV_LEN - recvd, 0);
+        if (reval == -1) {
+            perror("Error reading stream message");
+            return -1;
+        }
+        recvd += reval;
+    } while (reval > 0);
+
+    // IV 추출
+    memcpy(iv, buffer, IV_LEN);
+
+    // 데이터 복호화
+    int decrypted_len = decrypt_data(buffer + IV_LEN, recvd - IV_LEN, key, iv, decrypted_message);
+    if (decrypted_len == -1) {
+        printf("Error decrypting message from client %d\n", connectionfd);
+        // 복호화에 실패했지만 연결은 유지합니다.
+    } else {
+        printf("Client %d says: '%s'\n", connectionfd, decrypted_message);
+    }
+
+    return 0; // 연결 종료 대신 0을 반환하여 함수를 정상적으로 종료합니다.
+}
+
+
 
 // 서버를 실행하는 함수
 int run_server(Server server) {
@@ -80,21 +125,24 @@ int run_server(Server server) {
       return -1;
     }
 
-    if (handle_connection(connectionfd) == -1) {
+    if (handle_connection(connectionfd, server.key) == -1) {
       return -1;
     }
   }
 }
 
 int main(int argc, char* argv[]) {
-  if (argc != 2) {
-    printf("Usage: ./server port_num\n");
+  if (argc != 3) {
+    printf("Usage: ./server port_num encryption_key\n");
     return 1;
   }
 
   Server server;
   server.port = atoi(argv[1]);
   server.queue = 10;
+
+  // 암호화 키 설정
+  strncpy((char *)server.key, argv[2], sizeof(server.key));
 
   if (run_server(server) == -1) {
     return 1;
